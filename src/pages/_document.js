e65536acba9f06bd833aa9dd5d16c87490986cd7
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-01-25 17:29:04 -0300

    Initial template

---
+++ src/pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;400;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ src/pages/chat-room.js
@@ -0,0 +1,7 @@
+function ChatRoom() {
+  return (
+    <h1>Chat Room</h1>
+  )
+}
+
+export default ChatRoom
