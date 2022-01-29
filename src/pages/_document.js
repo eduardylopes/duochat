138742544cb755f715400684ffc6bf456879d3d4
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-01-29 00:26:14 -0300

    Fix vercel deploy

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,8 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700;800;900&display=swap" rel="stylesheet" />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
